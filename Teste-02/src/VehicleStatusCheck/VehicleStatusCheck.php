d6b77bc332682f9eb27b451c08812aec5b410c6f
João Henrique <joaohh41@hotmail.com>
2026-05-26 21:03:31 -0300

    Refactor checkStatus method for improved readability and consistency

--- Teste-02/src/VehicleStatusCheck/VehicleStatusCheck.php
+++ Teste-02/src/VehicleStatusCheck/VehicleStatusCheck.php
@@ -4,15 +4,15 @@ namespace App\VehicleStatusCheck;
 
 class VehicleStatusCheck
 {
-    public function checkStatus(int $quilometers, int $monthsBeforeLastMaintenance) : string
+    public function checkStatus(int $quilometers, int $monthsBeforeLastMaintenance): string
     {
-       if( $quilometers > 200_000 ){
+        if ($quilometers > 200_000) {
             return "Aposentado";
-       }
-       elseif ( !($quilometers > 200_000) && $monthsBeforeLastMaintenance > 12){
+        }
+
+        if ($monthsBeforeLastMaintenance > 12) {
             return "Em Manutenção";
-       }
-       
-       return "Disponível";
+        }
+        return "Disponível";
     }
 }
